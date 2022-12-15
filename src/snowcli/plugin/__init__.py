033dbcb38852430ae5e8a87d05be4e3482145fbc
Mark Keller <mark.keller@snowflake.com>
2022-12-14 17:52:51 -0800

    adding a plugin helper function

--- src/snowcli/plugin/__init__.py
+++ src/snowcli/plugin/__init__.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import pluggy
+import typer
 
 from . import hookspecs
 
@@ -11,3 +12,12 @@ hookimpl = pluggy.HookimplMarker(NAME)
 # Create plugin manager and set it up
 pm = pluggy.PluginManager(NAME)
 pm.add_hookspecs(hookspecs)
+
+
+def create_default_typer(**kwargs) -> typer.Typer:
+    """Function that creates a default typer class with SnowCLI's defaults."""
+    t = typer.Typer(
+        context_settings={"help_option_names": ["-h", "--help"]},
+        **kwargs,
+    )
+    return t
